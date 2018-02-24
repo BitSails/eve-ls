fd1dc880fbae4bf08f93233cf302514ba79953bf
pi <sweetie.pi@users.noreply.github.com>
2018-02-24 18:08:41 +0000

    Finished insertion

--- List.cpp
+++ List.cpp
@@ -18,14 +18,42 @@ class Node // This self-referential class stores integer data
 
 Node* frontPtr=nullptr;//initialize linked-list to be empty
 
-
+//.val is the data to insert
+// k is the position to insert at
+insert(int val, int k)
+{
 //inserting to the front
 //step 1
 Node* newPtr = new Node{val};
 
+if (k == 1)
+{
 //step 2
 newPtr->link = frontPtr; //preserve what frontPtr is pointing to
 
 //step 3
 frontPtr = newPtr; //update the front of the linked-list
+return; //quit the function
+}
+else //insert at another location
+{
+ Node* iPtr=nullptr;
+int i = 1;
 
+//step 4 (get a pointer to the (k-1)th node
+for ( iPtr= frontPtr; i < k-1; i++)
+ {
+ iPtr = iPtr->link;   
+  } 
+
+//step 5
+newPtr->link = iPtr ->link; 
+
+
+//step 6
+iPtr->link = newPtr;
+
+}
+
+
+}
